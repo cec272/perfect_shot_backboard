71f0e7a87b543e23102c015ca29aebc1c67eea7c
HarrisonAlan <hh559@cornell.edu>
2020-12-10 17:31:27 -0500

    Geometric variables

---
+++ Final Product/Python/Geometric_Variables.py
@@ -0,0 +1,87 @@
+# Harrison Hidalgo
+# ECE 5725 - Final Project
+# 
+
+#   Back |-----------------------------------|
+#        |                                   |
+# -------|----------------------.            |
+# Gy     |                      |G           |
+# |  ----|-------------.        |            |
+# |alpha1|             |B1      |            |
+# | | ---|----.        |        |            |   /\
+# | | mu1|    |P1      |        |            | y | 
+# | |  | |    |        |        |            |   |   x
+# -------|-----------------------------------|    --->
+#        |lam1|        |        |
+#        |-----beta1---|        |
+#        |--------Gx------------|
+#   Key:
+#       P = Attachment point from motors
+#       B = Constraint attachment
+#       G = Center of mass of backboard
+
+
+## Positions on backboard
+# Number of points: 3
+# Number of constriants: 1
+# Number of CoMs: 1
+
+# X positions
+lam1 =0.04001
+lam2 =0.22543
+lam3 =0.41085
+beta1=
+Gx   =0.2286
+# Y positions
+mu1   = 0.015875
+mu2   = 0.180975
+mu3   = 0.015875
+alpha1=
+Gy    = 0.098425
+
+#### Other dimensions
+### Gears 
+## Positions
+# Gear 1
+gear2_11
+gear2_12
+gear2_13
+
+# Gear 2
+gear2_21
+gear2_22
+gear2_23
+
+# Gear 3
+gear2_31
+gear2_32
+gear2_33
+
+# Sizes and effieciency
+r1  = 0.0100075
+r2  = 0.0227075
+r3  = 0.03302
+eta = 1
+
+### Motors
+## Unit vectors for motors
+# Motor 1
+motor_up11
+motor_up12
+motor_up13
+
+# Motor 2
+motor_up21
+motor_up22
+motor_up23
+
+# Motor 3
+motor_up31
+motor_up32
+motor_up33
+
+## Initial thetas
+th1_10 = 
+th1_20 = 
+th3_30 = 
+
