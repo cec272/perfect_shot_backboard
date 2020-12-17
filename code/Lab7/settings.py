3fb6748d588168dbf12f7f27553c808988f06a76
cec272 <cec272@cornell.edu>
2020-12-16 22:19:40 -0500

    delete fast robots code

--- code/Lab7/settings.py
+++
@@ -1,5 +0,0 @@
-Settings = {"adapter": "hci0",  # Not used in Windows and macOS
-            "OutputRawData": False,
-            "pingLoop": True,
-            "cached": "66:77:88:23:BB:EF"
-            }
